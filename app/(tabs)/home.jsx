6e92823a3112257a8756278a740ff05f697fad88
Alex <kovalevalex36@gmail.com>
2024-05-06 17:45:35 -0700

    model works + animations

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,9 +4,12 @@ import { Suspense } from 'react'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import InfoBar from '../components/InfoBar'
 import Model from '../components/Model'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { PerspectiveCamera } from '@react-three/drei/native'
 const Home = () => {
   return (
     <View>
+        <SafeAreaView>
         <ScrollView>
             {/* Header */}
             <View className="w-full flex flex-row justify-end p-2">
@@ -14,11 +17,23 @@ const Home = () => {
             </View>
 
             <View className="w-full h-[70vh] p-2">
-                <InfoBar style="absolute top-0 right-0"/>
-                <Canvas>
-                    <color attach="background" args={["#512DA8"]} />
-                    <Suspense>
-                    <Model/>
+                <InfoBar isOverlay={true}/>
+                <Canvas
+                camera={{ position: [0, 0, 8], fov: 60 }}
+                onCreated={(state) => {
+                    const _gl = state.gl.getContext();
+                    const pixelStorei = _gl.pixelStorei.bind(_gl);
+                    _gl.pixelStorei = function (...args) {
+                    const [parameter] = args;
+                    switch (parameter) {
+                    case _gl.UNPACK_FLIP_Y_WEBGL:
+                        return pixelStorei(...args);
+                    }
+                };
+                }}>
+                <ambientLight intensity={1.5} />
+                    <Suspense fallback={null}>
+                        <Model/>
                     </Suspense>
                 </Canvas>
             </View>
@@ -28,6 +43,7 @@ const Home = () => {
                 <Text>Global</Text>
             </View>
         </ScrollView>
+        </SafeAreaView>
     </View>
   )
 }
--- app/components/InfoBar.jsx
+++ app/components/InfoBar.jsx
@@ -1,16 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
-const InfoBar = (style) => {
+const InfoBar = (isOverlay) => {
+  console.log("hi")
   return (
-    <>
-    <View className={`${style}`}>
+    <View className={`absolute z-10 top-[20vh] left-2`}>
         <FontAwesome5 name="heart" size={30} color="black"/>
         <FontAwesome5 name="bolt" size={30} color="black"/>
         <FontAwesome5 name="hamburger" size={30} color="black"/>
         <FontAwesome5 name="mug-hot" size={30} color="black"/>
     </View>
-    </>
   )
 }
 
